9d38789c4506070938af071d519ece5f3bfcc6ce
Peyton Moss <pmoss94@gmail.com>
2022-10-20 12:40:34 -0600

    starting thursday

--- server/server.js
+++ server/server.js
@@ -7,6 +7,15 @@ app.use(express.json())
 app.use(cors())
 app.use(express.static('client'))
 
+let Rollbar = require('rollbar')
+let rollbar = new Rollbar({
+    accessToken: '31a1f2d38a2b462889b536bdb5865058',
+    caputureUncaught: true,
+    captureUnhandledRejections: true,
+})
+
+rollbar.log('Hello world!')
+
 const { getHTML, getCSS, getJS } = require('./controller')
 
 app.get('/', getHTML)
